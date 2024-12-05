5f0096007758a9a8ebc540a441b43e1b0d31a8ac
ALUNO <ALUNO@senaiescola.fsa>
2024-12-05 08:46:04 -0300

    alteracao 1

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -102,8 +102,6 @@ class TelaPrincipal extends StatelessWidget {
   }
 }
 
-// As classes das páginas de cadastro e histórico médico
-
 class CadastrarPetPage extends StatefulWidget {
   const CadastrarPetPage({super.key});
 
@@ -123,100 +121,48 @@ class _CadastrarPetPageState extends State<CadastrarPetPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Cadastrar Pet')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Campo de nome do dono
-              Container(
-                width: 300, // Largura do campo de digitação
-                child: TextField(
-                  controller: _nomeDonoController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nome do Dono',
-                    border: OutlineInputBorder(),
-                  ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
+              crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
+              children: [
+                _buildTextField(_nomeDonoController, 'Nome do Tutor'),
+                const SizedBox(height: 16),
+                _buildTextField(_cpfDonoController, 'CPF do Tutor', isNumber: true),
+                const SizedBox(height: 16),
+                _buildTextField(_nomePetController, 'Nome do Pet'),
+                const SizedBox(height: 16),
+                _buildTextField(_especiePetController, 'Espécie do Pet'),
+                const SizedBox(height: 16),
+                _buildTextField(_racaPetController, 'Raça do Pet'),
+                const SizedBox(height: 16),
+                _buildTextField(_dataNascimentoPetController, 'Data de Nascimento do Pet'),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _cadastrarPet,
+                  child: const Text('Cadastrar'),
                 ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo de CPF do dono
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: _cpfDonoController,
-                  decoration: const InputDecoration(
-                    labelText: 'CPF do Dono',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo de nome do pet
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: _nomePetController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nome do Pet',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo de espécie do pet
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: _especiePetController,
-                  decoration: const InputDecoration(
-                    labelText: 'Espécie do Pet',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo de raça do pet
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: _racaPetController,
-                  decoration: const InputDecoration(
-                    labelText: 'Raça do Pet',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo de data de nascimento do pet
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: _dataNascimentoPetController,
-                  decoration: const InputDecoration(
-                    labelText: 'Data de Nascimento do Pet',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.datetime,
-                ),
-              ),
-              const SizedBox(height: 24),
-
-              // Botão de envio
-              ElevatedButton(
-                onPressed: _cadastrarPet,
-                child: const Text('Cadastrar'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label, {bool isNumber = false}) {
+    return Container(
+      width: 300, // Largura do campo de digitação
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+        ),
+        keyboardType: isNumber ? TextInputType.number : TextInputType.text,
       ),
     );
   }
@@ -237,12 +183,10 @@ class _CadastrarPetPageState extends State<CadastrarPetPage> {
         dataNascimentoPet.isNotEmpty) {
       // Adicione a lógica para salvar os dados do pet aqui
 
-      // Exibe uma mensagem de sucesso ou navega para outra tela, se necessário
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Pet cadastrado com sucesso!')),
       );
     } else {
-      // Adiciona mensagem de erro se algum campo estiver vazio
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Por favor, preencha todos os campos.')),
       );
@@ -270,99 +214,6 @@ class HistoricoMedicoPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Histórico Médico')),
       body: const Center(child: Text('Tela com histórico médico do pet')),
-    );
-  }
-}
-
-class CadastrarDonoPage extends StatefulWidget {
-  const CadastrarDonoPage({super.key});
-
-  @override
-  State<CadastrarDonoPage> createState() => _CadastrarDonoPageState();
-}
-
-class _CadastrarDonoPageState extends State<CadastrarDonoPage> {
-  final TextEditingController _nomeController = TextEditingController();
-  final TextEditingController _localizacaoController = TextEditingController();
-  final TextEditingController _telefoneController = TextEditingController();
-  final TextEditingController _nomePetController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Cadastrar Dono')),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 16),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    controller: _nomeController,
-                    decoration: const InputDecoration(
-                      labelText: 'Nome do Dono',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    controller: _localizacaoController,
-                    decoration: const InputDecoration(
-                      labelText: 'Localização',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    controller: _telefoneController,
-                    decoration: const InputDecoration(
-                      labelText: 'Telefone',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.phone,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  width: 300,
-                  child: TextField(
-                    controller: _nomePetController,
-                    decoration: const InputDecoration(
-                      labelText: 'Nome do Pet',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Container(
-                  width: 300,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Lógica para cadastrar dono.
-                      // Pode incluir validações e armazenamento em banco de dados.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Dono cadastrado com sucesso!')),
-                      );
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cadastrar Dono'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
